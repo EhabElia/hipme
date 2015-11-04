407e91a94a19cd16a0e552fd694d2507e5cbe826
Ehab Elia <ehab.elia@gmail.com>
2015-11-04 17:40:18 +0100

    nothing

--- app/assets/javascripts/navbar_animation.js
+++ app/assets/javascripts/navbar_animation.js
@@ -1,6 +1,6 @@
 $(function(){
   $(window).on('scroll', function(e){
-    if ($(this).scrollTop() > 250) {
+    if ($(this).scrollTop() > 200) {
       $('.navbar-wagon').css('margin-top', '-70px');
     } else {
       $('.navbar-wagon').css('margin-top', '0px');
