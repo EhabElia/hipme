821dba6662ac80dd0ecfaa781cc94da569221bf5
Ehab Elia <ehab.elia@gmail.com>
2015-11-06 16:51:18 +0100

    login-flash-message-fixxed

--- app/assets/javascripts/navbar_animation.js
+++ app/assets/javascripts/navbar_animation.js
@@ -1,6 +1,6 @@
 $(function(){
   $(window).on('scroll', function(e){
-    if ($(this).scrollTop() > 120) {
+    if ($(this).scrollTop() > 10) {
       $('.navbar-wagon').css('margin-top', '-70px');
     } else {
       $('.navbar-wagon').css('margin-top', '0px');
